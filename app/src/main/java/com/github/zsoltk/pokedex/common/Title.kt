9b4a0a6660ac3db1957de6fd5df03422ea0248ad
Zsolt Kocsi <zsolt.kocsi@gmail.com>
2020-01-12 14:33:14 +0000

    Fine-tune Title

--- app/src/main/java/com/github/zsoltk/pokedex/common/Title.kt
+++ app/src/main/java/com/github/zsoltk/pokedex/common/Title.kt
@@ -1,23 +1,24 @@
 package com.github.zsoltk.pokedex.common
 
 import androidx.compose.Composable
-import androidx.compose.unaryPlus
 import androidx.ui.core.Text
 import androidx.ui.core.dp
+import androidx.ui.core.sp
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Spacing
-import androidx.ui.material.MaterialTheme
+import androidx.ui.text.TextStyle
+import androidx.ui.text.font.FontFamily
 import androidx.ui.text.font.FontWeight
 
 @Composable
 fun Title(text: String, color: Color) {
-    val typography = +MaterialTheme.typography()
-
     Text(
         text = text,
-        style = typography.h4.copy(
-            color = color,
-            fontWeight = FontWeight.Bold
+        style = TextStyle(
+            fontFamily = FontFamily("Roboto"),
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = color
         ),
         modifier = Spacing(
             top = 64.dp,
